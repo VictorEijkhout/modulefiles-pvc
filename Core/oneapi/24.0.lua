d20f2d704b8b88a37e26061bd3e5ab16b6012fc4
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-03-22 15:58:17 -0400

    set missing variables

--- Core/oneapi/24.0.lua
+++ Core/oneapi/24.0.lua
@@ -4,5 +4,15 @@ local compiler = "oneapi"
 local MP_ROOT  = os.getenv("VICTOR_MODULEPATH_ROOT")
 local version  = "24.0"
 
+setenv( "TACC_FAMILY_COMPILER", "oneapi" )
+setenv( "TACC_FAMILY_COMPILER_VERSION", "24.0" )
+
+setenv( "TACC_CC","icx" )
+setenv( "TACC_CXX","icpx" )
+setenv( "TACC_FC","ifx" )
+
+setenv( "TACC_SYSTEM","pvc" )
+setenv( "TACC_SYSTEM_CODE","spr" )
+
 prepend_path("MODULEPATH", pathJoin(MP_ROOT, "Compiler",compiler,version))
 family("compiler")
