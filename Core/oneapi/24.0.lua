008a85fce9d6bff406e52b4b1d5dcc7498ba8ca2
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-03-22 15:44:36 -0400

    start on 1api compiler

---
+++ Core/oneapi/24.0.lua
@@ -0,0 +1,8 @@
+inherit()
+
+local compiler = "oneapi"
+local MP_ROOT  = os.getenv("VICTOR_MODULEPATH_ROOT")
+local version  = "24.0"
+
+prepend_path("MODULEPATH", pathJoin(MP_ROOT, "Compiler",compiler,version))
+family("compiler")
